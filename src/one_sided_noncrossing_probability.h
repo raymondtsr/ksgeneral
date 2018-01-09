cdcb3b590b6109698ec3ba6e53b04fe0da3a83cd
raymondtsr <32581436+raymondtsr@users.noreply.github.com>
2018-01-09 12:59:08 +0000

    Delete one_sided_noncrossing_probability.h

--- src/one_sided_noncrossing_probability.h
+++
@@ -1,9 +0,0 @@
-#ifndef __one_sided_noncrossing_probabiity_h__
-#define __one_sided_noncrossing_probabiity_h__
-
-#include <vector>
-
-double ecdf_lower_noncrossing_probability(int n, const std::vector<double>& lower_bound_steps);
-double ecdf_upper_noncrossing_probability(int n, const std::vector<double>& upper_bound_steps);
-
-#endif
